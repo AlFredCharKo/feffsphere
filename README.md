0727ed2e07ef0c65a7db8bd62fc153e2414b1ded
Alexander Kompch <alexander.kompch@web.de>
2016-03-11 18:05:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cut a spherical cluster from .pdb or new-coo file around given atom for FEFF.inp
---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  feffsphere2
+//
+//  Created by Alexander Kompch on 11.03.16.
+//  Copyright (c) 2016 Alexander Kompch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
